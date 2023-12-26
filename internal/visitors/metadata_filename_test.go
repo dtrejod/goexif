c0cc5aac7fdba56b4d7e9b48befc3267ec000767
Devin Trejo <dtrejod@users.noreply.github.com>
2023-12-26 10:55:32 -0500

    Comment out mod time test for now.

--- internal/visitors/metadata_filename_test.go
+++ internal/visitors/metadata_filename_test.go
@@ -24,18 +24,19 @@ func TestMetadataFilename(t *testing.T) {
 		assert.Equal(t, expected, actual)
 	})
 
-	t.Run("with last modified timestamp", func(t *testing.T) {
-		expected := "2001/01/01/noexif.png"
-		srcMedia, err := mediatype.ID("./testdata/noexif.png", false)
-		assert.NoError(t, err)
+	// TODO(dtrejo): Fix mod time test. Git doesn't preserve the mod time of files making this test more involved
+	//t.Run("with last modified timestamp", func(t *testing.T) {
+	//	expected := "2001/01/01/noexif.png"
+	//	srcMedia, err := mediatype.ID("./testdata/noexif.png", false)
+	//	assert.NoError(t, err)
 
-		visitorFunc := NewMediaMetadataFilename(ctx, toPtr("."), true, false, false)
-		visitor := mediatype.FormatWithVisitor[string](srcMedia)
-		actual, err := visitor.Accept(ctx, visitorFunc)
-		assert.NoError(t, err)
+	//	visitorFunc := NewMediaMetadataFilename(ctx, toPtr("."), true, false, false)
+	//	visitor := mediatype.FormatWithVisitor[string](srcMedia)
+	//	actual, err := visitor.Accept(ctx, visitorFunc)
+	//	assert.NoError(t, err)
 
-		assert.Equal(t, expected, actual)
-	})
+	//	assert.Equal(t, expected, actual)
+	//})
 
 	t.Run("with timestamp as filename", func(t *testing.T) {
 		expected := "2000/01/01/946684800.png"
